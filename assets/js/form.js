1aa3163cce60ab52c1037431ffe95655a1e5d526
Ashley Hayes <ashleymh060504@gmail.com>
2024-10-24 14:25:07 -0400

    continued to work on js for blog form and list

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,5 @@
 // TODO: Create a variable that selects the form element
-const form = document.getElementByID('blogForm');
+const form = document.getElementById('blogForm');
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 function handleFormSubmission(event) {
   event.preventDefault();
@@ -19,11 +19,14 @@ function handleFormSubmission(event) {
   let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
     blogPosts.push(blogPost);
     localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
-  redirectPage();
+  redirectPage(`./blog.html`);
 }  
-// function redirectPage() {
-//   window.location.href = 'blog.html';
-// }
+let redirectURL = '';
+
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
 form.addEventListener('submit', handleFormSubmission);
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -39,17 +39,18 @@ toggle.addEventListener('change', () => {
 });
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
-
+function readLocalStorage() {
+  const data = localStorage.getItem(blogPosts);
+  return data ? JSON.parse(data) : [];
+}
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
-
+function storeLocalStorage(newBlogPost) {
+  let existingPosts = JSON.parse(localStorage.getItem(`blogPosts`)) || [];
+  existingPosts.push(newBlogPost);
+  localStorage.setItem(`blogPosts`, JSON.stringify(existingPosts));
+}
 
 // ! Use the following function whenever you need to redirect to a different page
 
-let redirectURL = '';
-
-const redirectPage = function (url) {
-  redirectURL = url;
-  location.assign(url);
-};
 
